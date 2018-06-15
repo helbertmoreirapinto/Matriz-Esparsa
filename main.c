5a386a7a047a3187d744d8b1965d9306456588e7
Helbert Moreira Pinto <helbert.tec.info@gmail.com>
2018-06-14 22:05:45 -0300

    Mudancas no codigo
    Lista Cruzada

--- main.c
+++ main.c
@@ -1,170 +1,203 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 
-struct Casa{
+struct Celula{
+    int x;
+    int y;
     float val;
-    unsigned long long int x;
-    unsigned long long int y;
-    struct Casa *ant;
-    struct Casa *prox;
+    struct Celula *proxX;
+    struct Celula *antX;
+    struct Celula *proxY;
+    struct Celula *antY;
 };
-typedef struct Casa Casa;
+typedef struct Celula Celula;
+typedef Celula* Celula_PTR;
 
-/* Funcoes */
-int main();
-Casa* criar_casa(unsigned long long int, unsigned long long int, float);
-void inserir_casa(Casa*, Casa**, Casa**);
-Casa* consultar_casa(Casa*, unsigned long long int, unsigned long long int);
+void inserir_celula(Celula_PTR, Celula_PTR*, Celula_PTR*);
+Celula_PTR criar_celula(int, int, float);
+float consultar_celula(int, int, Celula_PTR);
+
+void inserir_celula(Celula_PTR elem, Celula_PTR *headerX, Celula_PTR *headerY){
+    printf("[%d,%d] = %.2f\n\n", elem->x, elem->y, elem->val);
+
+    Celula_PTR auxHeaderX, auxHeaderY;
+    Celula_PTR newHeaderX, newHeaderY;
+    Celula_PTR listaX, listaY;
+
+    if(!*headerX || !*headerY){
+        /*lista vazia, criar Header X e Y*/
+        newHeaderX = criar_celula(elem->x, -1, 0);
+        newHeaderX->proxY = elem;
+        *headerX = newHeaderX;
+        newHeaderY = criar_celula(-1, elem->y, 0);
+        newHeaderY->proxX = elem;
+        *headerY = newHeaderY;
+    }else{
+        auxHeaderX = *headerX;
+        while(auxHeaderX){
+            if(elem->x < auxHeaderX->x && !auxHeaderX->proxX){
+                /*elem esta antes de auxHeaderX mas nao tem mais elementos na fila*/
+                /*Criar e inserir HeaderX ANTES de auxHeaderX*/
+                newHeaderX = criar_celula(elem->x, -1, 0);
+                newHeaderX->proxY = elem;
+                newHeaderX->proxX = auxHeaderX;
+                newHeaderX->antX = auxHeaderX->antX;
+                if(auxHeaderX->antX)
+                    auxHeaderX->antX->proxX = newHeaderX;
+                else
+                    *headerX = newHeaderX;      /*Se nao tinha anterior, elem eh o primeiro HeaderX*/
+                auxHeaderX->antX = newHeaderX;
+            }else if(elem->x > auxHeaderX->x){
+                /*a lista rodou, passou o valor de elem e não achou HeaderX com seu valor*/
+                /*Criar e inserir HeaderX DEPOIS de auxHeaderX*/
+                newHeaderX = criar_celula(elem->x, -1, 0);
+                newHeaderX->proxY = elem;
+                newHeaderX->antX = auxHeaderX;
+                newHeaderX->proxX = auxHeaderX->proxX;
+                if(auxHeaderX->proxX)
+                    auxHeaderX->proxX->antX = newHeaderX;
+                auxHeaderX->proxX = newHeaderX;
+                break;
+            }else if(elem->x == auxHeaderX->x){
+                /*Achou HeaderX com o valor*/
+                listaY = auxHeaderX;
+                while(listaY){
+                    if(elem->y < listaY->y && !listaY->proxY){
+                        /*insere elem ANTES de lista*/
+                        elem->antY = listaY->antY;
+                        elem->proxY = listaY;
+                        listaY->antY->proxY = elem;
+                        listaY->antY = elem;
+                        return;
+                    }else if(elem->y > listaY->y){
+                        /*insere elem DEPOIS de lista*/
+                        listaY->proxY = elem;
+                        elem->antY = listaY;
+                        return;
+                    }else if(elem->y == listaY->y){
+                        printf("Celula ocupada com o valor%.2f\n",listaY->val);
+                        return;
+                    }
+                    listaY = listaY->proxY; /*roda o prox da lista em X*/
+                }
+            }
+            auxHeaderX = auxHeaderX->proxX; /*roda o prox da lista em Y*/
+        }
+
+        auxHeaderY = *headerY;
+        while(auxHeaderY){
+            if(elem->y < auxHeaderY->y && !auxHeaderY->proxY){
+                /*Criar e inserir HeaderY ANTES de auxHeaderY*/
+                newHeaderY = criar_celula(-1, elem->y, 0);
+                newHeaderY->proxX = elem;
+                newHeaderY->proxY = auxHeaderY;
+                newHeaderY->antY = auxHeaderY->antY;
+                if(auxHeaderY->antY)
+                    auxHeaderY->antY->proxY = newHeaderY;
+                else
+                    *headerY = newHeaderY;
+                auxHeaderY->antY = newHeaderY;
+            }else if(elem->y > auxHeaderY->y){
+                /*Criar e inserir HeaderY DEPOIS de auxHeaderY*/
+                newHeaderY = criar_celula(-1, elem->y, 0);
+                newHeaderY->proxX = elem;
+                newHeaderY->antY = auxHeaderY;
+                auxHeaderY->proxY = newHeaderY;
+                if(auxHeaderY->proxY)
+                    auxHeaderY->proxY->antY = newHeaderY;
+                auxHeaderY->proxY = newHeaderY;
+                break;
+            }else if(elem->y == auxHeaderY->y){
+                /*Achou HeaderY com o valor*/
+                listaX = auxHeaderY;
+                while(listaX){
+                    if(elem->x < listaX->x && !listaX->proxX){
+                        /*insere elem ANTES de lista*/
+                        elem->antX = listaX->antX;
+                        elem->proxX = listaX;
+                        listaX->antX->proxX = elem;
+                        listaX->antX = elem;
+                        return;
+                    }else if(elem->x > listaX->x){
+                        /*insere elem DEPOIS de lista*/
+                        listaX->proxX = elem;
+                        elem->antX = listaX;
+                        return;
+                    }else if(elem->x == listaX->x){
+                        return;
+                    }
+                    listaX = listaX->proxX; /*roda o prox da lista em X*/
+                }
+            }
+            auxHeaderY = auxHeaderY->proxY; /*roda o prox da lista em Y*/
+        }
+    }
+}
+
+Celula_PTR criar_celula(int x, int y, float val){
+    Celula_PTR ret = (Celula_PTR)malloc(sizeof(Celula));
+    if(ret){
+        ret->val = val;
+        ret->x = x;
+        ret->y = y;
+        ret->proxX = NULL;
+        ret->proxY = NULL;
+        ret->antX = NULL;
+        ret->antY = NULL;
+    }else{
+        printf("Problema ao alocar Memoria\n");
+        exit(10);
+    }
+    return ret;
+}
+
+float consultar_celula(int x, int y, Celula_PTR header){
+    Celula_PTR aux;
+    while(header){
+        aux = header;
+        printf("\nX:%d ", header->x);
+        if(header->x == x){
+            while(header){
+                printf("Y:%d ",header->y);
+                if(header->y == y)
+                    return header->val;
+                header = header->proxY;
+            }
+            break;
+        }
+        header = aux;
+        header = header->proxX;
+    }
+    return 0;
+}
 
 int main(){
-    char respUsu;
-    char delConfirm;
-    unsigned long long int m = 0, n = 0, x, y;
+    int m, n;
+    int x, y;
+    int op;
     float val;
-    Casa *first = NULL;
-    Casa *last = NULL;
-    Casa *aux;
+    Celula_PTR firstX = NULL, firstY = NULL;
+    Celula_PTR elem;
 
-    do{
-        printf("|************************|\n");
-        printf("|         Matriz         |\n");
-        printf("|************************|\n");
-        if(!m || !n)
-            printf("|Inicializar matriz  | 1 |\n");
-        else{
-            printf("|Excluir matriz      | 2 |\n");
-            printf("|Adicionar valor     | 3 |\n");
-            printf("|Consultar valor     | 4 |\n");
-            printf("|Somar linha         | 5 |\n");
-            printf("|Somar coluna        | 6 |\n");
-        }
-        printf("|Sair                | 0 |\n");
-        printf("|************************|\n");
-        printf("|Selecione: ");
-        setbuf(stdin, NULL);
-        fscanf(stdin, "%c", &respUsu);
+    /*Criar matriz*/
+    printf("M: ");setbuf(stdin,NULL);scanf("%d",&m);
+    printf("N: ");setbuf(stdin,NULL);scanf("%d",&n);
+    while(1){
+        printf("opcao:\n1 - Inserir\n2 - Consultar\n");setbuf(stdin,NULL);scanf("%d",&op);
+        if(op==1){
+            /*Criar celula*/
+            printf("x: ");setbuf(stdin,NULL);scanf("%d",&x);
+            printf("y: ");setbuf(stdin,NULL);scanf("%d",&y);
+            printf("val: ");setbuf(stdin,NULL);scanf("%f",&val);
 
-        if(respUsu == '1' && (!m || !n)){
-            setbuf(stdin,NULL); printf("Insira valor de M: ");fscanf(stdin, "%I64u", &m);
-            setbuf(stdin,NULL); printf("Insira valor de N: ");fscanf(stdin, "%I64u", &n);
-        }else if((respUsu >= '2' && respUsu <= '6') && (m && n)){
-            switch(respUsu){
-            case '2':
-                do{
-                    setbuf(stdin, NULL);
-                    printf("Deseja excluir matriz? [S/N] ");fscanf(stdin, "%c", &delConfirm);
-                }while(delConfirm!='S' && delConfirm!='s' && delConfirm!='N' && delConfirm!='n');
-
-                if(delConfirm == 'S' || delConfirm == 's'){
-                    m = n = 0;
-                    first = last = NULL;
-                }
-                break;
-            case '3':
-                do{
-                    setbuf(stdin, NULL); printf("Poscicao [x y]: ");fscanf(stdin, "%I64u %I64u", &x, &y);
-                    setbuf(stdin, NULL); printf("Valor: ");fscanf(stdin, "%.2f", &val);
-                }while((x < 0 || x >= m) || (y < 0 || y >= n));
-                inserir_casa(criar_casa(x,y,val), &first, &last);
-                break;
-            case '4':
-                do{
-                    setbuf(stdin, NULL); printf("Poscicao [x y]: ");fscanf(stdin, "%I64u %I64u", &x, &y);
-                    setbuf(stdin, NULL); printf("Valor: ");fscanf(stdin, "%f", &val);
-                }while((x < 0 || x >= m) || (y < 0 || y >= n));
-                aux = consultar_casa(first,x,y);
-                if(aux)
-                    printf("O valor na Posicao [%x,%y]: %f\n", x, y);
-                printf("O valor na Posicao [%x,%y]: 0\n", x, y);
-                break;
-            case '5':
-
-                break;
-            }
-        }
-
-        system("CLS");
-
-        /* Mensagens cabecalho */
-        if(respUsu < '0' || respUsu > '5')
-            printf("Valor invalido\n");
-        else if(respUsu == '1')
-            printf("Matriz criada com sucesso!\n");
-        else if(respUsu == '2' && (delConfirm == 'S' || delConfirm == 's'))
-            printf("Matriz removida com sucesso!\n");
-    }while(respUsu != '0');
-}
-
-Casa* criar_casa(unsigned long long int x, unsigned long long int y, float val){
-    Casa *casa = malloc(sizeof(Casa));
-    if(casa){
-        casa->ant = NULL;
-        casa->prox = NULL;
-        casa->x = x;
-        casa->y = y;
-        casa->val = val;
-    }else{
-        printf("Memoria cheia\n");
-        getch();
-        exit(0);
-    }
-    return casa;
-}
-
-void inserir_casa(Casa *elem, Casa **first, Casa **last){
-    Casa *aux = consultar_casa((*first), elem->x, elem->y);
-    Casa *atual, *ant;
-    char updateConfirm;
-    if(aux){
-        printf("Posicao preenchida com o valor %f\n",aux->val);
-        do{
-            printf("Substituir valor na posicao? [S/N] ");
-            setbuf(stdin,NULL);
-            fscanf(stdin, "%c", &updateConfirm);
-        }while(updateConfirm != 'S' && updateConfirm != 'N' && updateConfirm != 's' && updateConfirm != 'n');
-        if(updateConfirm == 'S' || updateConfirm == 's')
-            aux->val = elem->val;
-    }else{
-        if(*first){
-            atual = *first;
-			ant = NULL;
-			while(atual){
-				if(atual->x < elem->x || ( (atual->x == elem->x)&&(atual->y < elem->y) )){
-					ant = atual;
-					atual = atual->prox;
-				}else{
-					if(atual->ant){
-						atual->ant->prox = elem;
-						elem->prox = atual;
-                        elem->ant = atual->ant;
-						atual->ant = elem;
-						return;
-					}
-					elem->prox = atual;
-					elem->ant = NULL;
-					atual->ant = elem;
-					*first = elem;
-					return;
-				}
-			}
-            ant->prox = elem;
-            elem->prox = NULL;
-            elem->ant = ant;
-            *last = elem;
-        }else{
-            *first = elem;
-            *last = elem;
+            elem = criar_celula(x,y,val);
+            inserir_celula(elem, &firstX, &firstY);
+        }else if(op == 2){
+            /*Consultar celula*/
+            printf("x: ");setbuf(stdin,NULL);scanf("%d",&x);
+            printf("y: ");setbuf(stdin,NULL);scanf("%d",&y);
+            val = consultar_celula(x,y,firstX);
+            printf("val: %.2f\n", val);
         }
     }
-}
-
-Casa* consultar_casa(Casa *first, unsigned long long int x, unsigned long long int y){
-    Casa *current = first;
-	while(current){
-		if(x == current->x && y == current->y)
-			return current;
-		current = current->prox;
-	}
-	return NULL;
 }
